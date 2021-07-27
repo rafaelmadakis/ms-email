8a1d119cd4cf8473f50c7172f796ea4f6af46b8d
Rafael Madakis <rafaelmadakis@bol.com.br>
2021-07-26 23:43:27 -0300

    Criaçao do service

---
+++ email/src/main/java/com/ms/email/service/EmailService.java
@@ -0,0 +1,12 @@
+package com.ms.email.service;
+
+import com.ms.email.repository.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private EmailRepository emailRepository;
+}
